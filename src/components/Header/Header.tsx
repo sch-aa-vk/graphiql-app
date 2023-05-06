e732e7ff088f499d1dea190debe322691297cd6d
EvgeniaM6 <evgminukova6@gmail.com>
2023-05-07 00:03:40 +0300

    feat: create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,59 @@
+import { MutableRefObject, useRef, useState } from 'react';
+import NavAuth from './NavAuth';
+import NavAnonim from './NavAnonim';
+
+function Header() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [isBurgerMenuOpened, setIsBurgerMenuOpened] = useState(false);
+  const burgerMenu: MutableRefObject<HTMLElement | null> = useRef(null);
+
+  function handleChange(input: HTMLInputElement) {
+    const isAuthorized: boolean = input.checked;
+    setIsAuth(isAuthorized);
+  }
+
+  function toggleBurgerMenu() {
+    if (isBurgerMenuOpened) {
+      burgerMenu.current?.classList.remove('opened');
+      setIsBurgerMenuOpened(false);
+      return;
+    }
+
+    burgerMenu.current?.classList.add('opened');
+    setIsBurgerMenuOpened(true);
+  }
+
+  return (
+    <>
+      <header className="header">
+        <div className="wrapper">
+          <div className="header__container">
+            <h1 className="logo">
+              <span className="logo__text">GraphiQL</span>
+              <span className="logo__text">Countries</span>
+            </h1>
+            <nav className="header__nav navigation" ref={burgerMenu}>
+              <button type="button" className="cross" aria-label="Close" onClick={toggleBurgerMenu}>
+                &#215;
+              </button>
+              {isAuth ? <NavAuth /> : <NavAnonim />}
+            </nav>
+            <button type="button" aria-label="Open" className="burger" onClick={toggleBurgerMenu}>
+              <div className="burger__content">
+                <span className="burger__line" />
+              </div>
+            </button>
+          </div>
+        </div>
+      </header>
+      <div className="toggle-auth">
+        <label htmlFor="auth">
+          <input type="checkbox" name="auth" id="auth" onChange={(e) => handleChange(e.target)} />
+          <span>auth</span>
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default Header;
