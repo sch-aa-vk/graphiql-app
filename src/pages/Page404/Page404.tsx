8adb28739d53b3e33dd645076d83c24b16143a1f
sch-aa-vk <sch.aa.vk@gmail.com>
2023-05-12 15:29:07 +0600

    feat: add styles to page404

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,5 +1,18 @@
+// import cat from '../../assets/images/kitty.png';
+// import cat from '../../assets/images/wrench.png';
+import cat from '../../assets/images/cat.png';
+
 function Page404() {
-  return <div className="undefined-page">Page 404</div>;
+  return (
+    <div className="undefined-page">
+      <div className="undefined-page__block">
+        <p className="undefined-page__text undefined-page__text-huge">4</p>
+        <img src={cat} alt="cat icon" />
+        <p className="undefined-page__text undefined-page__text-huge">4</p>
+      </div>
+      <p className="undefined-page__text">Page Not Found</p>
+    </div>
+  );
 }
 
 export default Page404;
