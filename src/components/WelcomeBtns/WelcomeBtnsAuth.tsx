8da77303243d97eca38c10f85176934e0dab51e4
EvgeniaM6 <evgminukova6@gmail.com>
2023-05-15 23:07:41 +0300

    fix: add click handling for button on welcome page

--- src/components/WelcomeBtns/WelcomeBtnsAuth.tsx
+++ src/components/WelcomeBtns/WelcomeBtnsAuth.tsx
@@ -1,11 +1,17 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 function WelcomeBtnsAuth() {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/workspace');
+  };
 
   return (
     <div className="auth-block">
-      <button type="button" className="auth-block__btn">
+      <button type="button" className="auth-block__btn" onClick={handleClick}>
         {t('mainLink')}
       </button>
     </div>
