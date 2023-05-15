dabd221791c7fd3dc49b963c815c5be4ccb04dad
sch-aa-vk <sch.aa.vk@gmail.com>
2023-05-15 23:25:34 +0600

    fix: fix rebase errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? '/' : '/graphiql-app/',
   plugins: [react()],
 });
