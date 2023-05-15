42d4c693f67d12830cf3d2f6e0698e5ea19a651d
sch-aa-vk <sch.aa.vk@gmail.com>
2023-05-15 22:13:54 +0600

    fix: fix error

--- src/components/WelcomeBtns/WelcomeBtnsAnonim.tsx
+++ src/components/WelcomeBtns/WelcomeBtnsAnonim.tsx
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
 function WelcomeBtnsAnonim() {
+  const { t } = useTranslation();
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -11,21 +12,7 @@ function WelcomeBtnsAnonim() {
   return (
     <div className="auth-block">
       <button type="button" className="auth-block__btn" onClick={handleClick}>
-        Sign in / Sign Up
-=======
-import { useTranslation } from 'react-i18next';
-
-function WelcomeBtnsAnonim() {
-  const { t } = useTranslation();
-
-  return (
-    <div className="auth-block">
-      <button type="button" className="auth-block__btn">
-        {t('loginLink')}
-      </button>
-      <button type="button" className="auth-block__btn">
-        {t('signupLink')}
->>>>>>> 073bacc (feat: add translation to welcome page)
+        {`${t('loginLink')} / ${t('signupLink')}`}
       </button>
     </div>
   );
