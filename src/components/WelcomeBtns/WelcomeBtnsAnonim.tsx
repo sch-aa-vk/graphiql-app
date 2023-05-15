6f7683a59712061b917016141fa238f0191e9835
sch-aa-vk <sch.aa.vk@gmail.com>
2023-05-15 22:08:40 +0600

    feat: add welcome page translation

--- src/components/WelcomeBtns/WelcomeBtnsAnonim.tsx
+++ src/components/WelcomeBtns/WelcomeBtnsAnonim.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useNavigate } from 'react-router-dom';
 
 function WelcomeBtnsAnonim() {
@@ -11,6 +12,20 @@ function WelcomeBtnsAnonim() {
     <div className="auth-block">
       <button type="button" className="auth-block__btn" onClick={handleClick}>
         Sign in / Sign Up
+=======
+import { useTranslation } from 'react-i18next';
+
+function WelcomeBtnsAnonim() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="auth-block">
+      <button type="button" className="auth-block__btn">
+        {t('loginLink')}
+      </button>
+      <button type="button" className="auth-block__btn">
+        {t('signupLink')}
+>>>>>>> 073bacc (feat: add translation to welcome page)
       </button>
     </div>
   );
