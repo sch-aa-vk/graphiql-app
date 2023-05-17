552d163bb19f95d04f3b474faddb531796fa9365
sch-aa-vk <sch.aa.vk@gmail.com>
2023-05-17 13:42:50 +0600

    feat: add server, proxy to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? '/' : '/graphiql-app/',
+  base: '/',
+  server: {
+    proxy: {
+      '/graphql': {
+        target: 'https://countries.trevorblades.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 });
