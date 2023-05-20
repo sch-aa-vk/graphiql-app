a28e0ec37119de14dedb830eabbe9edf41af25cc
EvgeniaM6 <evgminukova6@gmail.com>
2023-05-20 11:54:54 +0300

    feat: add nested schema objects array to store, add models

--- src/store/workspaceSlice.ts
+++ src/store/workspaceSlice.ts
@@ -1,18 +1,11 @@
 /* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
-
-interface Workspace {
-  docsPanelVisible: boolean;
-  docsFetched: boolean;
-}
-
-interface State {
-  workspace: Workspace;
-}
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { State, TNestedObjs, Workspace } from '../models';
 
 const initialState: Workspace = {
   docsPanelVisible: false,
   docsFetched: true,
+  nestedObjsArr: [],
 };
 
 const workspaceSlice = createSlice({
@@ -22,12 +15,23 @@ const workspaceSlice = createSlice({
     docsClick: (state) => {
       state.docsPanelVisible = !state.docsPanelVisible;
     },
+    addNestedObj: (state, action: PayloadAction<TNestedObjs>) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      state.nestedObjsArr.push(action.payload);
+    },
+    removeLastNestedObj: (state) => {
+      const newNestedObjsArr = [...state.nestedObjsArr];
+      newNestedObjsArr.splice(state.nestedObjsArr.length - 1, 1);
+      state.nestedObjsArr = newNestedObjsArr;
+    },
   },
 });
 
-export const { docsClick } = workspaceSlice.actions;
+export const { docsClick, addNestedObj, removeLastNestedObj } = workspaceSlice.actions;
 
 export const docsPanelVisible = (state: State) => state.workspace.docsPanelVisible;
 export const docsFetched = (state: State) => state.workspace.docsFetched;
+export const nestedObjsArr = (state: State) => state.workspace.nestedObjsArr;
 
 export default workspaceSlice.reducer;
