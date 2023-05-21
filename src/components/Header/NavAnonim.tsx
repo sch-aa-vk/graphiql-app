350b6965b335690ab86cada86bd40e0a6f81f69e
EvgeniaM6 <evgminukova6@gmail.com>
2023-05-21 12:34:39 +0300

    feat: add language-select component to header

--- src/components/Header/NavAnonim.tsx
+++ src/components/Header/NavAnonim.tsx
@@ -1,11 +1,17 @@
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import LanguageSelect from '../LanguageSelect/LanguageSelect';
 
 function NavAnonim() {
   const { t } = useTranslation();
 
   return (
     <ul className="navigation__list">
+      <li className="navigation__item">
+        <div className="language">
+          <LanguageSelect />
+        </div>
+      </li>
       <li className="navigation__item">
         <NavLink to="/" className="navigation__link">
           {t('homeLink')}
