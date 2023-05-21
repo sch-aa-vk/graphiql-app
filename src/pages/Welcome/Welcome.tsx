11df44227716920cfab3e0ebfc5e87a4b72c753f
EvgeniaM6 <evgminukova6@gmail.com>
2023-05-21 17:59:21 +0300

    feat: add information about team member

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -23,11 +23,7 @@ function Welcome() {
                 devName={t('amina')}
                 ghAvatarLink="https://avatars.githubusercontent.com/u/89934145?v=4"
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus, urna vel
-                bibendum euismod, nunc diam fermentum diam, eu tincidunt erat odio non lorem.
-                facilisi. Proin finibus pellentesque metus at eleifend. Vivamus sed neque tortor.
-                tortor. Nullam vulputate porta massa non elementum. Cras molestie ipsum in viverra
-                viverra luctus. In gravida.
+                {t('aminaDescription')}
               </DeveloperInfo>
               <DeveloperInfo
                 ghLink="https://github.com/redcliphaloe"
@@ -35,11 +31,7 @@ function Welcome() {
                 devName={t('artem')}
                 ghAvatarLink="https://avatars.githubusercontent.com/u/14157545?v=4"
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus, urna vel
-                bibendum euismod, nunc diam fermentum diam, eu tincidunt erat odio non lorem.
-                facilisi. Proin finibus pellentesque metus at eleifend. Vivamus sed neque tortor.
-                tortor. Nullam vulputate porta massa non elementum. Cras molestie ipsum in viverra
-                viverra luctus. In gravida.
+                {t('artemDescription')}
               </DeveloperInfo>
               <DeveloperInfo
                 ghLink="https://github.com/EvgeniaM6"
@@ -47,43 +39,19 @@ function Welcome() {
                 devName={t('yevheniia')}
                 ghAvatarLink="https://avatars.githubusercontent.com/u/93492831?v=4"
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus, urna vel
-                bibendum euismod, nunc diam fermentum diam, eu tincidunt erat odio non lorem.
-                facilisi. Proin finibus pellentesque metus at eleifend. Vivamus sed neque tortor.
-                tortor. Nullam vulputate porta massa non elementum. Cras molestie ipsum in viverra
-                viverra luctus. In gravida.
+                {t('yevheniiaDescription')}
               </DeveloperInfo>
             </div>
             <h2 className="project-info__title">{t('aboutProject')}</h2>
             <div className="project-info__container flex-reverse">
-              <div className="project-info__content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam molestie mi magna,
-                vitae vitae fermentum ligula pharetra sit amet. Vivamus sapien massa, pharetra ut
-                hendrerit nec, dignissim eu leo. Donec vulputate nisi at eros facilisis ullamcorper.
-                Mauris in congue tortor. Duis dictum bibendum odio eu tempor. Suspendisse tellus
-                tellus, convallis non laoreet nec, auctor id dolor. Donec sit amet varius erat.
-                Phasellus semper dui in laoreet consectetur. Donec eu mattis libero. Sed vel lorem
-                rhoncus, placerat nunc ut, lacinia libero. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Fusce blandit, ante a dapibus fermentum, dui odio ultricies elit,
-                erat elit ac enim.
-              </div>
+              <div className="project-info__content">{t('aboutProjectText')}</div>
               <div className="project-info__image">
                 <img src={projectImageObj} alt="project" className="project-info__image-img" />
               </div>
             </div>
             <h2 className="project-info__title">{t('aboutCourse')}</h2>
             <div className="project-info__container">
-              <div className="project-info__content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam molestie mi magna,
-                vitae vitae fermentum ligula pharetra sit amet. Vivamus sapien massa, pharetra ut
-                hendrerit nec, dignissim eu leo. Donec vulputate nisi at eros facilisis ullamcorper.
-                Mauris in congue tortor. Duis dictum bibendum odio eu tempor. Suspendisse tellus
-                tellus, convallis non laoreet nec, auctor id dolor. Donec sit amet varius erat.
-                Phasellus semper dui in laoreet consectetur. Donec eu mattis libero. Sed vel lorem
-                rhoncus, placerat nunc ut, lacinia libero. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Fusce blandit, ante a dapibus fermentum, dui odio ultricies elit,
-                sed sed posuere erat elit ac enim.
-              </div>
+              <div className="project-info__content">{t('aboutCourseText')}</div>
               <div className="project-info__image">
                 <img
                   src={rssImageObj}
